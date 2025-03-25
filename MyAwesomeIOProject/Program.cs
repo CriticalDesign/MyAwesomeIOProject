cfd4f14bba555684ae1c14744102f5f58311872e
AARON LANGILLE <aaron.langille@cambriancollege.ca>
2025-03-25 13:53:18 -0400

    New message..

--- MyAwesomeIOProject/Program.cs
+++ MyAwesomeIOProject/Program.cs
@@ -11,6 +11,7 @@
             Console.WriteLine("IOT is my fave class ever!");
             Console.WriteLine("IOT is my fave class ever!");
             Console.WriteLine("I am in class right now!");
+            Console.WriteLine("Sandip always listens in class!");
         }
     }
 }

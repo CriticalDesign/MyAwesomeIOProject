36df788d6466698d112aff3e4c91353038b769e2
AARON LANGILLE <aaron.langille@cambriancollege.ca>
2025-03-25 13:41:02 -0400

    Add project files.

---
+++ MyAwesomeIOProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyAwesomeIOProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

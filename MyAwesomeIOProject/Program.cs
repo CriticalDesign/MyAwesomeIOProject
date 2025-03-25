f77dd399a26a3cb8e75d78ed348d1d076d1bb4a7
AARON LANGILLE <aaron.langille@cambriancollege.ca>
2025-03-25 13:43:08 -0400

    Changed output to be more personal and depressing.

--- MyAwesomeIOProject/Program.cs
+++ MyAwesomeIOProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I could have done anything with my life. Oddly enough, I chose to do this. :/");
         }
     }
 }
